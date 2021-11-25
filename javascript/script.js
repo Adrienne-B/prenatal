93bc7e80d0ea1004b29811c76e19568b2e0d4c7e
adrienne_b <WorldofArts_95@yahoo.com>
2021-11-25 11:29:36 -0600

    Mobile size, having problems with questions and concerns section.

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+const todoList = document.querySelector(".list");
+const button = document.querySelector(".more");
+
+console.log("abc");
+
+var list = [
+	"Eros in cursus turpis massa  dui ut ornare lectus. Ac auctor augue mauris gravida in fermentum.",
+	
+	"Eros in cursus turpis massa  dui ut ornare lectus. Ac auctor augue mauris gravida in fermentum.",
+	
+	"Eros in cursus turpis massa  dui ut ornare lectus. Ac auctor augue mauris gravida in fermentum.",
+	
+	"Eros in cursus turpis massa  dui ut ornare lectus. Ac auctor augue mauris gravida in fermentum."
+];
+
+console.log("reviews");
+
+alert("this is a test");
